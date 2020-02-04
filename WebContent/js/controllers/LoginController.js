692f4ee0b7038c17cd85f5d4bbd123e8745c511e
bharath_maska <20809168+InCarNaTeDuDe@users.noreply.github.com>
2020-02-04 20:03:16 +0530

    Update LoginController.js

--- WebContent/js/controllers/LoginController.js
+++ WebContent/js/controllers/LoginController.js
@@ -1,6 +1,6 @@
 (function() {
     "use strict";
-    angular.module("hangingcafe").controller('LoginController', ['$scope', function($scope) {
+    angular.module("hangingcafe").controller('LoginController', ['$scope','$http', function($scope,$http) {
         $scope.ui={
             content: 'form'
         };
@@ -12,13 +12,9 @@
         $scope.getHelp = function(n,num){
             if(n && num){
                 $scope.ui.content = 'loader';
-                fetch('http://limitless-garden-41603.herokuapp.com/get-help?name='+n+'&phone='+num)
-                    .then(function(json){
-                        return json.json();
-                    }).then(function(data){
-                        setTimeout(function(){
-                            $scope.ui.content = 'form';
-                        });
+                $http.get('http://limitless-garden-41603.herokuapp.com/get-help?name='+n+'&phone='+num)
+                    .then(function(data){
+                        $scope.ui.content = 'form';
                         console.log("from API:",data); 
                     }).catch(function(e){
                         conosle.error("Error",e);
