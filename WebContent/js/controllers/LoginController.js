9dafb3076ed8720b8265e3283d68b323efe34e3f
bharath_maska <20809168+InCarNaTeDuDe@users.noreply.github.com>
2020-02-04 19:41:13 +0530

    Update LoginController.js

--- WebContent/js/controllers/LoginController.js
+++ WebContent/js/controllers/LoginController.js
@@ -2,7 +2,9 @@
     "use strict";
     angular.module("hangingcafe").controller('LoginController', ['$scope', function($scope) {
         $scope.text = "Hello login please!";
-        $scope.ui={toLoad:false};
+        $scope.ui={
+            content: 'form'
+        };
         $scope.login = function() {
         	if(!this.loginForm.$valid){
         		alert("Please fill in the fields!");
@@ -10,12 +12,12 @@
         };
         $scope.getHelp = function(n,num){
             if(n && num){
-                $scope.ui.toLoad = true;
+                $scope.ui.content = 'loader';
                 fetch('http://limitless-garden-41603.herokuapp.com/get-help?name='+n+'&phone='+num)
                     .then(function(json){
                         return json.json();
                     }).then(function(data){
-                        $scope.ui.toLoad = false;
+                        $scope.ui.content = 'form';
                         console.log("from API:",data); 
                     }).catch(function(e){
                         conosle.error("Error",e);
