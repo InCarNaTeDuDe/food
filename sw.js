3aa89de6ee9a3747fd114ae514be2c602655f209
bharath_maska <20809168+InCarNaTeDuDe@users.noreply.github.com>
2020-02-18 23:36:31 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const cacheName = "cache-v1";
+
+const resourcesToCache = [
+    '/', '/index.html'
+];
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(resourcesToCache);
+            })
+    );
+});
+
+/* Implementing Cache first Strategy */
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.match(event.request).then(cachedItem => {
+        return cachedItem || fetch(event.request);
+    }));
+});
